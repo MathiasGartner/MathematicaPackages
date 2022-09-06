bfb3f6da9fd452a39abb1dbc8858857e93c4a01b
mathiasgartner <mathias.gartner@jku.at>
2022-09-06 15:50:49 +0200

    use Arrayplot instead of MatrixPlot to fix color bar issues use OptionsPattern for customizing PlotStyle use PlotRangePadding->None for Plots

--- TDVMC/Plot.m
+++ TDVMC/Plot.m
@@ -22,9 +22,9 @@ PlotLastEk::usage = "plots E(k) for the last available timestamp";
 PlotLastRho::usage = "plots \[Rho](r) for the last available timestamp";
 PlotLastRho2::usage = "plots \!\(\*SubscriptBox[\(\[Rho]\), \(2\)]\)(\!\(\*SubscriptBox[\(r\), \(i\)]\), \!\(\*SubscriptBox[\(r\), \(j\)]\)) for the last available timestamp";
 
-PlotGrData::usage = "plots g_2(r_ij, t) as matrix plot and line plot";
-PlotSkData::usage = "plots S(k, t) as matrix plot and line plot";
-PlotRhoData::usage = "plots \[Rho](r, t) as matrix plot and line plot";
+PlotGrData::usage = "plots g_2(r_ij, t) as array plot and line plot";
+PlotSkData::usage = "plots S(k, t) as array plot and line plot";
+PlotRhoData::usage = "plots \[Rho](r, t) as array plot and line plot";
 
 PlotGrByPosition::usage = "plots g_2(r_ij, t) for fixed values of r_ij";
 
@@ -48,58 +48,100 @@ $PlotTheme={"Detailed"};
 
 SetOptions[$FrontEnd,PrintingStyleEnvironment->"Working"];
 
+SetOptions[{ArrayPlot,ListLinePlot},PlotRangePadding->None];
+
+
 imageSize = Medium;
 
-PlotSingleObservable[x_, y_, label_String, colIndex_:1, legend_:""]:=Module[{count},
-count = Min[Length[x], Length[y]];
-ListLinePlot[{x[[;;count]], y[[;;count]]}//Transpose, PlotRange->All, PlotLegends->If[legend=="", None, LineLegend[{color[colIndex]}, {legend}]], PlotLabel->label,  ImageSize->imageSize, PlotStyle->color[colIndex]]
+Clear[PlotSingleObservable];
+Options[PlotSingleObservable] = {"InsetFirstValue"->False, "InsetLastValue"->False, "PlotColor"->color[1]};
+PlotSingleObservable[x_, y_, label_String, opt:OptionsPattern[{PlotSingleObservable,Plot}]]:=PlotSingleObservable[x, y, label, "", opt];
+PlotSingleObservable[x_, y_, label_String, legend_, opt:OptionsPattern[{PlotSingleObservable,Plot}]]:=Module[{count, inset},
+	count = Min[Length[x], Length[y]];
+inset={};
+	If[OptionValue["InsetFirstValue"], AppendTo[inset, Inset["("<>ToString[x[[1]]]<>", "<>ToString[y[[1]]]<>")", Scaled[{0.2,0.8}]]]];
+If[OptionValue["InsetLastValue"], AppendTo[inset, Inset["("<>ToString[x[[-1]]]<>", "<>ToString[y[[-1]]]<>")", Scaled[{0.8,0.8}]]]];
+	ListLinePlot[{x[[;;count]], y[[;;count]]}//Transpose, 
+	PlotRange->All, 
+	PlotLegends->If[legend=="", None, LineLegend[{OptionValue["PlotColor"]}, {legend}]], 
+	PlotLabel->label,  
+	ImageSize->imageSize, 
+	PlotStyle->OptionValue["PlotColor"],
+	Epilog->inset,
+Evaluate@FilterRules[{opt},Options[Plot]]
+]
 ];
 PlotTemporalData[data_, times_, label_String]:=Module[{count},
 count = Min[Length[data//Transpose], Length[times]];
-ListLinePlot[TemporalData[data[[All, ;;count]], {times[[;;count]]}], PlotRange->All, PlotLegends->None, PlotLabel->label,PlotStyle->"Rainbow", ImageSize->imageSize]
+ListLinePlot[TemporalData[data[[All, ;;count]], {times[[;;count]]}], PlotRange->All, PlotLegends->None, PlotLabel->label,PlotStyle->"BlueGreenYellow", ImageSize->imageSize]
 ];
 
 PlotLists[data_, label_String]:=Module[{},
-ListLinePlot[data, PlotRange->All, PlotLegends->None, PlotLabel->label,PlotStyle->"Rainbow", ImageSize->imageSize]
+ListLinePlot[data, PlotRange->All, PlotLegends->None, PlotLabel->label,PlotStyle->"BlueGreenYellow", ImageSize->imageSize]
 ];
 
-PlotER[data_] := PlotSingleObservable[data["timesSystem"], data["eR"], "Re[E(t)]"];
-PlotER[data_, colIndex_] := PlotSingleObservable[data["timesSystem"], data["eR"], "Re[E(t)]", colIndex];
-PlotEI[data_] := PlotSingleObservable[data["timesSystem"], data["eI"], "Im[E(t)]"];
-PlotEKin[data_] := PlotSingleObservable[data["timesSystem"], data["other"][[1]], "Re[E_kin(t)]"];
-PlotEPot[data_] := PlotSingleObservable[data["timesSystem"], data["other"][[2]], "E_pot(t)"];
+Clear[PlotER];
+Options[PlotER] =  {"PlotColor"->color[1]};
+PlotER[data_, opt:OptionsPattern[]] := PlotSingleObservable[data["timesSystem"], data["eR"], "Re[E(t)]",  "InsetLastValue"->True, opt];
+PlotER[data_, colIndex_, opt:OptionsPattern[]] := PlotSingleObservable[data["timesSystem"], data["eR"], "Re[E(t)]", "", "InsetLastValue"->True, "PlotColor"->color[colIndex], opt];
+
+Clear[PlotEI];
+Options[PlotEI] =  {"PlotColor"->color[1]};
+PlotEI[data_, opt:OptionsPattern[]] := PlotSingleObservable[data["timesSystem"], data["eI"], "Im[E(t)]",  "InsetLastValue"->True, opt];
+
+Clear[PlotEKin];
+Options[PlotEKin] =  {"PlotColor"->color[1]};
+PlotEKin[data_, opt:OptionsPattern[]] := PlotSingleObservable[data["timesSystem"], data["other"][[1]], "Re[E_kin(t)]",  "InsetLastValue"->True, opt];
+
+Clear[PlotEPot];
+Options[PlotEPot] =  {"PlotColor"->color[1]};
+PlotEPot[data_, opt:OptionsPattern[]] := PlotSingleObservable[data["timesSystem"], data["other"][[2]], "E_pot(t)",  "InsetLastValue"->True, opt];
 
 PlotPR[data_] := PlotTemporalData[data["pR"][[;;-2]]//DS50, data["timesSystem"], "parameters real (t)"];
 PlotPI[data_] := PlotTemporalData[data["pI"][[;;-2]]//DS50, data["timesSystem"], "parameters imag (t)"];
 PlotPRForTimesteps[data_] := PlotLists[data["pR"][[;;-2]]//Transpose//DS10, "parmeters real (k)"]
 PlotPRDiffs[data_] := PlotPRDiffs[data, 50];
 PlotPRDiffs[data_, lastN_] := Module[{lastData},
-lastData = If[Length[data["timesSystem"]]> lastN,
+lastData = If[
+Length[data["timesSystem"]]> lastN,
 TakeData[data, -lastN;;-1;;1],
 data
 ];
 PlotTemporalData[(lastData["pR"][[;;-2]]-(lastData["pR"][[;;-2]]//Transpose)[[1]])//DS50, lastData["timesSystem"], "parameters real (t)"]
 ];
 
-PlotLastGr[data_/;MatchQ[data, KeyValuePattern["gr"->{{}}]]] := Nothing;
-PlotLastGr[data_]:= PlotSingleObservable[data["grGrid"], data["gr"]//Transpose//Last, "g_2(r_ij, t="<>ToString[data["timesAdditional"]//Last]<>")"];
-PlotLastSk[data_/;MatchQ[data, KeyValuePattern["sk"->{{}}]]] := Nothing;
-PlotLastSk[data_]:= PlotSingleObservable[data["skGrid"], data["sk"]//Transpose//Last, "S(k, t="<>ToString[data["timesAdditional"]//Last]<>")"];
-PlotLastEk[data_/;MatchQ[data, KeyValuePattern["sk"->{{}}]]] := Nothing;
-PlotLastEk[data_]:= PlotSingleObservable[data["skGrid"], ({data["skGrid"], data["sk"]//Transpose//Last}//Transpose)/.{k_, s_}-> k^2/s, "E(k, t="<>ToString[data["timesAdditional"]//Last]<>")"];
-PlotLastRho[data_/;MatchQ[data, KeyValuePattern["rho"->{{}}]]] := Nothing;
-PlotLastRho[data_]:= PlotSingleObservable[data["rhoGrid"], data["rho"]//Transpose//Last, "\[Rho](r, t="<>ToString[data["timesAdditional"]//Last]<>")"];
+Clear[PlotLastGr];
+Options[PlotLastGr] =  {"PlotColor"->color[1]};
+PlotLastGr[data_/;MatchQ[data, KeyValuePattern["gr"->{{}}]], opt:OptionsPattern[{PlotLastGr,Plot}]] := Nothing;
+PlotLastGr[data_ , opt:OptionsPattern[{PlotLastGr,Plot}]]:= PlotSingleObservable[data["grGrid"], data["gr"]//Transpose//Last, "g_2(r_ij, t="<>ToString[data["timesAdditional"]//Last]<>")", "InsetFirstValue"->True, "InsetLastValue"->True, opt];
+
+Clear[PlotLastSk];
+Options[PlotLastSk] =  {"PlotColor"->color[1]};
+PlotLastSk[data_/;MatchQ[data, KeyValuePattern["sk"->{{}}]], opt:OptionsPattern[]] := Nothing;
+PlotLastSk[data_, opt:OptionsPattern[]]:= PlotSingleObservable[data["skGrid"], data["sk"]//Transpose//Last, "S(k, t="<>ToString[data["timesAdditional"]//Last]<>")", opt];
+
+Clear[PlotLastEk];
+Options[PlotLastEk] =  {"PlotColor"->color[1]};
+PlotLastEk[data_/;MatchQ[data, KeyValuePattern["sk"->{{}}]], opt:OptionsPattern[]] := Nothing;
+PlotLastEk[data_, opt:OptionsPattern[]]:= PlotSingleObservable[data["skGrid"], ({data["skGrid"], data["sk"]//Transpose//Last}//Transpose)/.{k_, s_}-> k^2/s, "E(k, t="<>ToString[data["timesAdditional"]//Last]<>")", opt];
+
+Clear[PlotLastRho];
+Options[PlotLastRho] =  {"PlotColor"->color[1]};
+PlotLastRho[data_/;MatchQ[data, KeyValuePattern["rho"->{{}}]], opt:OptionsPattern[]] := Nothing;
+PlotLastRho[data_, opt:OptionsPattern[]]:= PlotSingleObservable[data["rhoGrid"], data["rho"]//Transpose//Last, "\[Rho](r, t="<>ToString[data["timesAdditional"]//Last]<>")", opt];
+
 PlotLastRho2[data_/;MatchQ[data, KeyValuePattern["rho2"->{{}}]]] := Nothing;
-PlotLastRho2[data_]:= Module[{rangeR1, rangeR2, range, dataRho, dataRho2, plotRho2, plotRho2Normalized, plots, t},
+PlotLastRho2[data_]:= Module[{rangeR1, rangeR2, range, dataRho, dataRho2, plotRho2, plotRho2Normalized, plots, t, gridLength},
 t=ToString[data["timesAdditional"]//Last];
 rangeR1 = {data["rho2Grid"][[1]]//First, data["rho2Grid"][[1]]//Last};
 rangeR2 = {data["rho2Grid"][[2]]//First, data["rho2Grid"][[2]]//Last};
 range = {rangeR1, rangeR2};
+gridLength = data["rho2Grid"][[1]]//Length;
 dataRho2 = data["rho2"]//Transpose//Last//ArrayReshape[#, Length/@data["rho2Grid"]]&//Reverse;
-dataRho = data["rho"]//Transpose//Last//KroneckerProduct[#, #]&;
-plotRho2 = MatrixPlot[dataRho2,ColorFunction->"Rainbow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"\[Rho]_2(r_i, r_j, t) @ t="<>t,FrameLabel->{{"r_i", ""}, {"r_j", ""}}, DataRange->range];
-plotRho2Normalized=MatrixPlot[dataRho2/dataRho,ColorFunction->"Rainbow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"\[Rho]_2(r_i, r_j, t) / (\[Rho](r_i, t)\[Rho](r_j, t)) @ t="<>t,FrameLabel->{{"r_i", ""}, {"r_j", ""}}, DataRange->range];
-plots = {plotRho2, plotRho2Normalized};
+dataRho = (Mean/@Partition[data["rho"]//Transpose//Last, 2])//KroneckerProduct[#, #]&;
+plotRho2 = ArrayPlot[dataRho2,ColorFunction->"BlueGreenYellow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"\[Rho]_2(r_i, r_j, t) @ t="<>t,FrameLabel->{{"r_i", ""}, {"r_j", ""}}, DataRange->range];
+plotRho2Normalized=ArrayPlot[dataRho2/dataRho,ColorFunction->"BlueGreenYellow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"\[Rho]_2(r_i, r_j, t) / (\[Rho](r_i, t)\[Rho](r_j, t)) @ t="<>t,FrameLabel->{{"r_i", ""}, {"r_j", ""}}, DataRange->range];
+plots = {plotRho2, plotRho2Normalized, dataRho2, dataRho};
 plots
 ];
 
@@ -110,10 +152,10 @@ rangeT = {data["timesAdditional"]//First, data["timesAdditional"]//Last};
 range = {rangeX, rangeT};
 tmp=(data["gr"]//Transpose//Reverse);
 diffs= ((#-tmp[[-1]])&)/@tmp;
-plotM=tmp//MatrixPlot[#,ColorFunction->"Rainbow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"g_2(r_ij, t)",FrameLabel->{{"r_ij", ""}, {"t", ""}}, DataRange->range]&;
-plotMD=diffs//MatrixPlot[#,ColorFunction->"Rainbow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize,PlotLabel->"g_2(r_ij, t) - g_2(r_ij, 0)", FrameLabel->{{"r_ij", ""}, {"t", ""}}, DataRange->range]&;
-plotL=tmp//Reverse//DS50//ListLinePlot[#, PlotRange->All, PlotStyle->"Rainbow", ImageSize->imageSize, PlotLabel->"g_2(r_ij, t)", DataRange->rangeX]&;
-plotLLog=tmp//Reverse//DS50//ListLogLinearPlot[#, PlotRange->All, PlotStyle->"Rainbow", Joined->True, ImageSize->imageSize, PlotLabel->"g_2(r_ij, t)", DataRange->rangeX]&;
+plotM=tmp//ArrayPlot[#,ColorFunction->"BlueGreenYellow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"g_2(r_ij, t)",FrameLabel->{{"r_ij", ""}, {"t", ""}}, DataRange->range]&;
+plotMD=diffs//ArrayPlot[#,ColorFunction->"BlueGreenYellow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize,PlotLabel->"g_2(r_ij, t) - g_2(r_ij, 0)", FrameLabel->{{"r_ij", ""}, {"t", ""}}, DataRange->range]&;
+plotL=tmp//Reverse//DS50//ListLinePlot[#, PlotRange->All, PlotStyle->"BlueGreenYellow", ImageSize->imageSize, PlotLabel->"g_2(r_ij, t)", DataRange->rangeX]&;
+plotLLog=tmp//Reverse//DS50//ListLogLinearPlot[#, PlotRange->All, PlotStyle->"BlueGreenYellow", Joined->True, ImageSize->imageSize, PlotLabel->"g_2(r_ij, t)", DataRange->rangeX]&;
 {plotM, plotMD, plotL, plotLLog}
 ];
 
@@ -124,32 +166,34 @@ rangeT = {data["timesAdditional"]//First, data["timesAdditional"]//Last};
 range = {rangeX, rangeT};
 tmp=(data["sk"]//Transpose//Reverse);
 diffs= ((#-tmp[[-1]])&)/@tmp;
-plotM=tmp//MatrixPlot[#,ColorFunction->"Rainbow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"S(k, t)",FrameLabel->{{"k", ""}, {"t", ""}}, DataRange->range]&;
-plotMD=diffs//MatrixPlot[#,ColorFunction->"Rainbow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize,PlotLabel->"S(k, t) - S(k, 0)", FrameLabel->{{"k", ""}, {"t", ""}}, DataRange->range]&;
-plotL=tmp//Reverse//DS50//ListLinePlot[#, PlotRange->All, PlotStyle->"Rainbow", ImageSize->imageSize, PlotLabel->"S(k, t)", DataRange->rangeX]&;
-plotLLog=tmp//Reverse//DS50//ListLogLinearPlot[#, PlotRange->All, PlotStyle->"Rainbow", Joined->True, ImageSize->imageSize, PlotLabel->"S(k, t)", DataRange->rangeX]&;
+plotM=tmp//ArrayPlot[#,ColorFunction->"BlueGreenYellow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"S(k, t)",FrameLabel->{{"k", ""}, {"t", ""}}, DataRange->range]&;
+plotMD=diffs//ArrayPlot[#,ColorFunction->"BlueGreenYellow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize,PlotLabel->"S(k, t) - S(k, 0)", FrameLabel->{{"k", ""}, {"t", ""}}, DataRange->range]&;
+plotL=tmp//Reverse//DS50//ListLinePlot[#, PlotRange->All, PlotStyle->"BlueGreenYellow", ImageSize->imageSize, PlotLabel->"S(k, t)", DataRange->rangeX]&;
+plotLLog=tmp//Reverse//DS50//ListLogLinearPlot[#, PlotRange->All, PlotStyle->"BlueGreenYellow", Joined->True, ImageSize->imageSize, PlotLabel->"S(k, t)", DataRange->rangeX]&;
 {plotM, plotMD, plotL, plotLLog}
 ];
 
-PlotRhoData[data_/;MatchQ[data, KeyValuePattern["rho"->{{}}]]] := Nothing;
+PlotRhoData[data_/;MatchQ[data, KeyValuePattern["rho"->{{}}]]] := Sequence;
 PlotRhoData[data_]:=Module[{tmp, diffs, plotM, plotMD, plotL, rangeX, rangeT, range},
 rangeX = {data["rhoGrid"]//First, data["rhoGrid"]//Last};
 rangeT = {data["timesAdditional"]//First, data["timesAdditional"]//Last};
 range = {rangeX, rangeT};
 tmp=(data["rho"]//Transpose//Reverse);
 diffs= ((#-tmp[[-1]])&)/@tmp;
-plotM=tmp//MatrixPlot[#,ColorFunction->"Rainbow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"\[Rho](r, t)",FrameLabel->{{"r", ""}, {"t", ""}}, DataRange->range]&;
-plotMD=diffs//MatrixPlot[#,ColorFunction->"Rainbow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize,PlotLabel->"\[Rho](r, t) - \[Rho](r, 0)", FrameLabel->{{"r", ""}, {"t", ""}}, DataRange->range]&;
-plotL=tmp//Reverse//DS50//ListLinePlot[#, PlotRange->All, PlotStyle->"Rainbow", ImageSize->imageSize, PlotLabel->"\[Rho](r, t)", DataRange->rangeX]&;
-{plotM, plotMD, plotL}
+plotM=tmp//ArrayPlot[#,ColorFunction->"BlueGreenYellow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize, PlotLabel->"\[Rho](r, t)",FrameLabel->{{"r", ""}, {"t", ""}}, DataRange->range]&;
+plotMD=diffs//ArrayPlot[#,ColorFunction->"BlueGreenYellow", AspectRatio->1, MaxPlotPoints->Infinity, ImageSize->imageSize,PlotLabel->"\[Rho](r, t) - \[Rho](r, 0)", FrameLabel->{{"r", ""}, {"t", ""}}, DataRange->range]&;
+plotL=tmp//Reverse//DS50//ListLinePlot[#, PlotRange->All, PlotStyle->"BlueGreenYellow", ImageSize->imageSize, PlotLabel->"\[Rho](r, t)", DataRange->rangeX]&;
+{{plotM, plotMD, plotL}, {tmp, diffs}}
 ];
 
-PlotGrByPosition[data_/;MatchQ[data, KeyValuePattern["gr"->{{}}]]] := Nothing;
-PlotGrByPosition[data_]:=Module[{plots, rangeX, rangeT, range},
+Clear[PlotGrByPosition];
+Options[PlotGrByPosition] =  {"PlotColor"->color[1]};
+PlotGrByPosition[data_/;MatchQ[data, KeyValuePattern["gr"->{{}}]], opt:OptionsPattern[]] := Nothing;
+PlotGrByPosition[data_, opt:OptionsPattern[]]:=Module[{plots, rangeX, rangeT, range},
 rangeX = {data["grGrid"]//First, data["grGrid"]//Last};
 rangeT = {data["timesAdditional"]//First, data["timesAdditional"]//Last};
 range = {rangeX, rangeT};
-plots = Map[ListLinePlot[#[[1]], PlotRange->All, FrameLabel->{{"", ""}, {"", "\!\(\*SubscriptBox[\(g\), \(2\)]\)(\!\(\*SubscriptBox[\(r\), \(ij\)]\), t)"}}, PlotLegends->Placed["\!\(\*SubscriptBox[\(r\), \(ij\)]\)="<>ToString[#[[2]]],  {0.5, 0.2}], DataRange->rangeT]&, {data["gr"]//DS20, data["grGrid"]//DS20}//Transpose];
+plots = Map[ListLinePlot[#[[1]], PlotRange->All, FrameLabel->{{"", ""}, {"", "\!\(\*SubscriptBox[\(g\), \(2\)]\)(\!\(\*SubscriptBox[\(r\), \(ij\)]\), t)"}}, PlotLegends->Placed["\!\(\*SubscriptBox[\(r\), \(ij\)]\)="<>ToString[#[[2]]],  {0.5, 0.2}], DataRange->rangeT, PlotStyle->OptionValue["PlotColor"]]&, {data["gr"]//DS20, data["grGrid"]//DS20}//Transpose];
 plots
 ];
 
@@ -173,17 +217,22 @@ animation = Manipulate[Show[plots[[i]], PlotRange->{All, {min - offset, max + of
 animation
 ];
 
-CompareER[data_] := Show[MapIndexed[PlotSingleObservable[#1["timesSystem"], #1["eR"], "Re[E(t)]", First[#2], FileNameTake[#1["directory"]]]&,data], PlotRange->All];
+CompareER[data_] := Module[{plots},
+plots = Show[MapIndexed[PlotSingleObservable[#1["timesSystem"], #1["eR"], "Re[E(t)]", FileNameTake[#1["directory"]], "PlotColor"->color[First[#2]]]&,data], PlotRange->All];
+plots
+];
 
-PlotAll[data_]:={
-{PlotER[data], PlotEI[data], PlotEKin[data], PlotEPot[data]},
-{PlotPR[data], PlotPI[data], PlotPRForTimesteps[data], PlotPRDiffs[data, 50]},
-{PlotLastGr[data], PlotLastSk[data], PlotLastEk[data], PlotLastRho[data]},
-PlotLastRho2[data],
-PlotGrData[data],
-PlotSkData[data],
-PlotRhoData[data],
-PlotGrByPosition[data]
+Clear[PlotAll];
+Options[PlotAll] =  {"PlotColor"->color[1]};
+PlotAll[data_, opt:OptionsPattern[]]:={
+	{PlotER[data, opt], PlotEI[data, opt], PlotEKin[data, opt], PlotEPot[data, opt]},
+	{PlotPR[data], PlotPI[data], PlotPRForTimesteps[data], PlotPRDiffs[data, 50]},
+	{PlotLastGr[data, opt], PlotLastSk[data, opt], PlotLastEk[data, opt], PlotLastRho[data, opt], PlotLastGr[data, ScalingFunctions->"Log", opt]},
+	PlotLastRho2[data],
+	PlotGrData[data],
+	PlotSkData[data],
+	(tmp=PlotRhoData[data]; If[Length[tmp]>0, tmp[[1]],Nothing]),
+	PlotGrByPosition[data, opt]
 };
 
 PlotAllForExport[data_]:= Module[{plots},
